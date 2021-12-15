d45f91cfc36189232a74122ee7f85724edf04673
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-12-15 12:14:26 -0500

    Implement Dino jump animation

--- dino.js
+++ dino.js
@@ -1,14 +1,18 @@
+import { incrementCustomProperty, getCustomProperty, setCustomProperty } from "./updateCustomProperty.js";
+
 let dinoElement = document.querySelector('[data-dino]');
 
 const JUMP_SPEED = 0.45;
-const GRAVITY = 0.011;
+const GRAVITY = 0.0015;
 const DINO_FRAME_COUNT = 2;
 const FRAME_TIME = 100;
 
 let isJumping;
 let dinoFrame;
 let currentFrameTime;
+let yVelocity;
 
+// Run Dino function
 const handleRun = (delta, speedScale) => {
   if (isJumping) {
     dinoElement.src = `images/dino-stationary.png`;
@@ -23,16 +27,38 @@ const handleRun = (delta, speedScale) => {
 
   currentFrameTime += delta * speedScale;
 }
-const handleJump = () => {
 
+// Jump Dino function
+const handleJump = (delta) => {
+  if (!isJumping) return;
+
+  incrementCustomProperty(dinoElement, '--bottom', yVelocity * delta)
+
+  if (getCustomProperty(dinoElement, '--bottom') <= 0) {
+    setCustomProperty(dinoElement, '--bottom', 0);
+    isJumping = false;
+  }
+
+  yVelocity -= GRAVITY * delta;
+}
+
+// Handle user press jump function
+const onJump = (e) => {
+  if (e.code !== 'Space' || isJumping) return;
+  yVelocity = JUMP_SPEED;
+  isJumping = true;
 }
 
 export const setupDino = () => {
   isJumping = false;
   dinoFrame = 0;
   currentFrameTime = 0;
+  yVelocity = 0;
+  setCustomProperty(dinoElement, '--bottom', 0);
+  document.removeEventListener('keydown', onJump);
+  document.addEventListener('keydown', onJump);
 }
 export const updateDino = (delta, speedScale) => {
   handleRun(delta, speedScale);
-  handleJump();
+  handleJump(delta);
 }
